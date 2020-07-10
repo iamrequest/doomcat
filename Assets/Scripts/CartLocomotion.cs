0c97f2454ea680e86ee3f979957a0ed44c528db0
request <iamrequest@protonmail.com>
2020-07-10 19:29:25 -0400

    Added cart locomotion, camera, and laser aiming
    - Cart locomotion is based off of this twitter post: https://twitter.com/Nitroneers/status/1238779466832363520
    	- Some work is still needed, to handle the case where the cart gets fliped.
    - Camera aim is based on CinemaMachine

---
+++ Assets/Scripts/CartLocomotion.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Fix physics when the car flips
+/// <summary>
+// Cart physics are based off of this twitter post: https://twitter.com/Nitroneers/status/1238779466832363520
+// TL;DR: The cart follows a sphere. 
+//        We add torque to the car to turn it left and right
+//        We add force to the sphere, rolling in the forward direction of the cart
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class CartLocomotion : MonoBehaviour {
+    public GameObject car;
+    private Rigidbody car_rb;
+    private Rigidbody rb;
+
+    public float forwardSpeed;
+    public float turnSpeed;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        car_rb = car.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        // Rotate the car
+        if (Input.GetKey(KeyCode.A)) {
+            car_rb.AddTorque(car.transform.up * -turnSpeed, ForceMode.Force);
+            //forwardDir += transform.right * -turnSpeed;
+        }  else if (Input.GetKey(KeyCode.D)) {
+            car_rb.AddTorque(car.transform.up * turnSpeed, ForceMode.Force);
+            //forwardDir += transform.right * turnSpeed;
+        } 
+        
+
+        // Roll the sphere, in the direction of the car's forward dir
+        if (Input.GetKey(KeyCode.W)) {
+            rb.AddForce(car.transform.forward * forwardSpeed, ForceMode.Force);
+        } else if (Input.GetKey(KeyCode.S)) {
+            rb.AddForce(car.transform.forward * -forwardSpeed, ForceMode.Force);
+        }
+
+        // Move the car towards the ball
+        car_rb.MovePosition(transform.position);
+
+        // Temp: Scale ball
+        if (Input.GetKeyDown(KeyCode.P)) {
+            transform.localScale *= 1.1f;
+        } else if (Input.GetKeyDown(KeyCode.O)) {
+            transform.localScale /= 1.1f;
+        }
+
+        // Temp: Re-orient everything upwards
+        if (Input.GetKey(KeyCode.Space)) {
+            car.transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAimer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimer : MonoBehaviour {
+    [Header("References")]
+    public GameObject cam;
+    public GameObject lookatTarget;
+    public GameObject laserSource;
+    public LineRenderer lineRenderer;
+    private Vector3 reticleTarget;
+
+    [Header("Aiming")]
+    public float maxAimDistance;
+    public LayerMask raycastLayermask;
+    public float laserSourceTurnSpeed;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        lineRenderer.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        UpdateLineRenderer();
+        laserSource.transform.LookAt(reticleTarget * laserSourceTurnSpeed, Vector3.up);
+    }
+
+    void UpdateLineRenderer() {
+        lineRenderer.SetPosition(0, laserSource.transform.position);
+
+        // -- Find the aim position
+        // Raycast from the lookat target, in the direction of the camera.
+        //  We start from the lookat target, to prevent any issues with obstructions between the camera and the lookat target
+        // If we collide with anything, that'll be the aim position 
+        // If we DON'T collide with anything, then just set the aim position to be "maxAimDistance" meters away
+        Vector3 raycastDir = (lookatTarget.transform.position - cam.transform.position).normalized;
+
+        RaycastHit hit;
+        if (Physics.Raycast(lookatTarget.transform.position, raycastDir, out hit, maxAimDistance, raycastLayermask)) {
+            // Something is in our reticle - aim at it
+            reticleTarget = hit.point;
+        } else {
+            // Nothing in our reticle. Just aim "maxAimDistance" units away
+            reticleTarget = cam.transform.position + raycastDir * maxAimDistance;
+        }
+
+        lineRenderer.SetPosition(1, reticleTarget);
+
+        Debug.DrawLine(lookatTarget.transform.position, reticleTarget, Color.green);
+        Debug.DrawLine(laserSource.transform.position, reticleTarget, Color.red);
+    }
+}
