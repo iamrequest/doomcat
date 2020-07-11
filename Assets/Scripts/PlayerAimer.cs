c7021f48c2f2434917cc6470e11c68923c85d406
request <iamrequest@protonmail.com>
2020-07-10 21:21:06 -0400

    Animated cat tail
    - Cat tail now moves to avoid shooting the cat, when the blaster and the cart are facing the same direction

--- Assets/Scripts/PlayerAimer.cs
+++ Assets/Scripts/PlayerAimer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAimer : MonoBehaviour {
+    private const bool DEBUG = false;
+
     [Header("References")]
     public GameObject cam;
     public GameObject lookatTarget;
@@ -13,7 +15,10 @@ public class PlayerAimer : MonoBehaviour {
     [Header("Aiming")]
     public float maxAimDistance;
     public LayerMask raycastLayermask;
-    public float laserSourceTurnSpeed;
+    public Vector3 baseTailOffset;
+    public float maxRearTailDistance, maxSideTailDistance;
+    public float minRearTailDistance, minSideTailDistance;
+    public float forwardSimilarityMaxThreshold, forwardSimilarityMinThreshold;
 
 
     // Start is called before the first frame update
@@ -23,20 +28,19 @@ public class PlayerAimer : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        CalculateReticlePosition();
+        UpdateLaserSource();
         UpdateLineRenderer();
-        laserSource.transform.LookAt(reticleTarget * laserSourceTurnSpeed, Vector3.up);
     }
 
-    void UpdateLineRenderer() {
-        lineRenderer.SetPosition(0, laserSource.transform.position);
-
-        // -- Find the aim position
-        // Raycast from the lookat target, in the direction of the camera.
-        //  We start from the lookat target, to prevent any issues with obstructions between the camera and the lookat target
-        // If we collide with anything, that'll be the aim position 
-        // If we DON'T collide with anything, then just set the aim position to be "maxAimDistance" meters away
+    // -- Find the aim position
+    // Raycast from the lookat target, in the direction of the camera.
+    //  We start from the lookat target, to prevent any issues with obstructions between the camera and the lookat target
+    void CalculateReticlePosition() {
         Vector3 raycastDir = (lookatTarget.transform.position - cam.transform.position).normalized;
 
+        // If we collide with anything, that'll be the aim position 
+        // If we DON'T collide with anything, then just set the aim position to be "maxAimDistance" meters away
         RaycastHit hit;
         if (Physics.Raycast(lookatTarget.transform.position, raycastDir, out hit, maxAimDistance, raycastLayermask)) {
             // Something is in our reticle - aim at it
@@ -45,10 +49,50 @@ public class PlayerAimer : MonoBehaviour {
             // Nothing in our reticle. Just aim "maxAimDistance" units away
             reticleTarget = cam.transform.position + raycastDir * maxAimDistance;
         }
+    }
 
+    // Move the laser source into position, and make it face the target
+    // If the target is infront of the cart, then the tail should move to the side (to avoid shooting the cat)
+    void UpdateLaserSource() {
+        // Calculate whether or not the laser is pointed in the same direction as the cart
+        // If the laser is pointed forward (and so is the cart), then move the tail to the side, and forwards.
+        // If the laser is pointed to the side or backwards, then move the tail to the center, and back
+        //
+        // forwardSimilarity: 1 if laser is pointed forwards, -1 if pointed backwards
+        // sideSimilarity: 1 if laser is pointed right, -1 if pointed left
+        float forwardSimilarity = Vector3.Dot(lookatTarget.transform.forward, cam.transform.forward);
+        float sideSimilarity = Vector3.Dot(lookatTarget.transform.right, cam.transform.forward);
+
+        // TODO: Fix snapping when laser faces forward
+        //if (forwardSimilarity > forwardSimilarityMaxThreshold) {
+        //    sideSimilarity = 1f;
+        //} else if (forwardSimilarity < forwardSimilarityMinThreshold) {
+        //    sideSimilarity = -1f;
+        //}
+
+        if (DEBUG) {
+            Debug.Log("Forward similarity: " + forwardSimilarity);
+            Debug.Log("Side similarity: " + sideSimilarity);
+        }
+
+
+        // Lerp between the calculated offsets, and add that to the base position
+        Vector3 tailLocalPos = baseTailOffset;
+        tailLocalPos.z += Mathf.Lerp(minRearTailDistance, maxRearTailDistance, forwardSimilarity);
+        tailLocalPos.x += Mathf.Lerp(minSideTailDistance, maxSideTailDistance, sideSimilarity);
+        laserSource.transform.localPosition = tailLocalPos;
+
+        // Make the blaster look at the target
+        laserSource.transform.LookAt(reticleTarget, Vector3.up);
+    }
+
+    void UpdateLineRenderer() {
+        lineRenderer.SetPosition(0, laserSource.transform.position);
         lineRenderer.SetPosition(1, reticleTarget);
 
-        Debug.DrawLine(lookatTarget.transform.position, reticleTarget, Color.green);
-        Debug.DrawLine(laserSource.transform.position, reticleTarget, Color.red);
+        if (DEBUG) {
+            Debug.DrawLine(lookatTarget.transform.position, reticleTarget, Color.green);
+            Debug.DrawLine(laserSource.transform.position, reticleTarget, Color.red);
+        }
     }
 }
