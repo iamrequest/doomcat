25656117eba490381cce93d4d6efa7d56d1e17fc
request <iamrequest@protonmail.com>
2020-07-12 15:15:34 -0400

    Added camera animation for main menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+    Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Switch() {
+        animator.SetTrigger("Switch");
+    }
+}
